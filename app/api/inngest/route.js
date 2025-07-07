b0c53be6b6c6d7a31a2950498cd87a4d9f58064d
Nirmalya Dash <nirmalyadash21@gmail.com>
2025-07-07 18:49:04 +0530

    Fixed login form bug and improved validation

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,5 +1,5 @@
 import { serve } from "inngest/next";
-import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
+import { createUserOrder, inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT} = serve({
@@ -9,7 +9,8 @@ export const { GET, POST, PUT} = serve({
    
     syncUserCreation,
     syncUserUpdation,
-    syncUserDeletion
+    syncUserDeletion,
+    createUserOrder
 
   ],
 });
---
+++ app/api/user/data/route.js
@@ -0,0 +1,26 @@
+import connectDB from "@/config/db"
+import { getAuth } from "@clerk/nextjs/server"
+import User from "@/models/User"
+import { NextResponse } from "next/server"
+
+export async function GET(request) {
+     try {
+
+        
+         const{userId} = getAuth(request)
+         console.log("Clerk userId:",userId);
+
+         await connectDB()
+         const user = await User.findById(userId)
+
+         if(!user){
+             return NextResponse.json({success:false , message: "User Not Found"})
+         }
+
+         return NextResponse.json({success:true , user})
+
+     } catch (error) {
+         return NextResponse.json({success:false , message: error.message })
+     }
+}
+   
